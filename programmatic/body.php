4eca46ac96770258364c7b814dbebe7923c42f7b
ajaythekkiniyi <ajaythekkiniyi@gmail.com>
2022-02-01 10:28:56 +0530

    first

---
+++ programmatic/body.php
@@ -0,0 +1,35 @@
+<!--
+Start of Floodlight Tag: Please do not remove
+Activity name of this tag: Amrita International SS Beta Page 9th June - Landing Page
+URL of the webpage where the tag is expected to be placed: 
+This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+Creation Date: 06/09/2021
+-->
+<script type="text/javascript">
+var axel = Math.random() + "";
+var a = axel * 10000000000000;
+document.write('<iframe src="https://10322129.fls.doubleclick.net/activityi;src=10322129;type=invmedia;cat=amrit00a;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+</script>
+<noscript>
+<iframe src="https://10322129.fls.doubleclick.net/activityi;src=10322129;type=invmedia;cat=amrit00a;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+</noscript>
+<!-- End of Floodlight Tag: Please do not remove -->
+
+
+
+<!--
+Start of Floodlight Tag: Please do not remove
+Activity name of this tag: Amrita International SS Beta Page 9th June - Submit
+URL of the webpage where the tag is expected to be placed: 
+This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+Creation Date: 06/09/2021
+-->
+<script type="text/javascript">
+var axel = Math.random() + "";
+var a = axel * 10000000000000;
+document.write('<iframe src="https://10322129.fls.doubleclick.net/activityi;src=10322129;type=invmedia;cat=amrit00b;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+</script>
+<noscript>
+<iframe src="https://10322129.fls.doubleclick.net/activityi;src=10322129;type=invmedia;cat=amrit00b;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+</noscript>
+<!-- End of Floodlight Tag: Please do not remove -->
